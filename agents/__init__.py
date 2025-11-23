c74f8e131fbf0b6243811c2b3d80309b6a74d57b
Mohsen Arjmandi <mohsen.arjmandi@gmail.com>
2025-11-23 18:11:14 +0100

    removed unneccary agent templates

--- agents/__init__.py
+++ agents/__init__.py
@@ -10,13 +10,9 @@ from .templates.langgraph_random_agent import LangGraphRandom
 from .templates.langgraph_thinking import LangGraphThinking
 from .templates.llm_agents import (
     LLM,
-    FastLLM,
-    GuidedLLM,
-    ReasoningLLM,
     SensiLLM,
 )
 from .templates.random_agent import Random
-from .templates.reasoning_agent import ReasoningAgent
 from .templates.smolagents import SmolCodingAgent, SmolVisionAgent
 from .sensi import Sensi
 
@@ -34,7 +30,6 @@ for rec in Recorder.list():
     AVAILABLE_AGENTS[rec] = Playback
 
 # update the agent dictionary to include subclasses of LLM class
-AVAILABLE_AGENTS["reasoningagent"] = ReasoningAgent
 AVAILABLE_AGENTS["sensillm"] = SensiLLM
 
 
